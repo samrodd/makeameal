112ed0965d36b8f71a4cef437f9ae4aaa80072c3
anndu1 <48729176+anndu1@users.noreply.github.com>
2021-06-21 15:40:56 -0400

    create basic page

---
+++ src/client/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Make A Meal</h1>
+
+      <input type="text" name="ingredient" />
+    </div>
+  );
+}
+
+export default App;
