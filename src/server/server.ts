4e09d0034cb1a73a6b8326380a94360a86ad16a2
Jinxiang-Lin <78502763+Jinxiang-Lin@users.noreply.github.com>
2021-06-21 11:50:00 -0400

    receive user input and return matched recipes id
    added dependences: body-parser, cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,9 +2,11 @@ const express = require('express');
 const mysql = require('mysql'); //mysql driver
 const app = express();
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+const cors = require("cors");
 dotenv.config();
 
-const port = 3000;
+const port = 3002;
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -17,15 +19,20 @@ connection.connect((err) => {
     if(err) throw err;
     console.log('Database connected!');
 })
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
-connection.query('SELECT * FROM full_dataset', function (err, rows, fields) {
-    if (err) throw err
-    
-    console.log('Row 1 is ', rows[0])
-  })
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.post('/api/post', (req, res) => {
+    const ingredientName = req.body.ingredientName;
+    //console.log(req.body.ingredientName);
+    //console.log(req.body);
+    let matched_id = [];
+    connection.query("SELECT id, ingredient_name FROM ingredient_occurrences WHERE ingredient_name = (?)", ingredientName, function(err, rows, fields){
+        if(err) throw err
+        console.log(rows);
+    })
+    console.log(ingredientName);
 })
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`);
