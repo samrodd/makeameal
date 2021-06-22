3deb0dcb7864a0bfd37d25ac7682a40106b03e63
anndu1 <48729176+anndu1@users.noreply.github.com>
2021-06-22 15:14:40 -0400

    added search bar

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Search from './search';
 
 function App() {
   return (
     <div className="App">
       <h1>Make A Meal</h1>
+      <h4>Create your meal now based on the ingredients you have at hand!</h4>
 
+      <Search />
       <input type="text" name="ingredient" />
     </div>
   );
