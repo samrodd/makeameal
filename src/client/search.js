03ad8cc5bf91e0964deea0dc93a4f1542f911142
anndu1 <48729176+anndu1@users.noreply.github.com>
2021-07-13 18:10:19 -0400

    updated search bar

--- src/client/search.js
+++ src/client/search.js
@@ -2,27 +2,75 @@ import "./App.css";
 import axios from "axios";
 import {useState, useEffect} from "react";
 
+
 const Search = () => {
     const [ingredientName, setIngredientName] = useState("");
+    const [matchedRecipes, setMatchedRecipes] = useState([]);
 
-    /*const submitIngredient = () => {
-         Axios.post('http://localhost:3001/api/post', {
+    useEffect(()=> {
+        axios.get('http://localhost:3001/api/get').then((response)=>{
+            console.log(response.data)
+            setMatchedRecipes(response.data)
+        })
+    }, [])
+    const submitIngredient = () => {
+         axios.post('http://localhost:3001/api/post', {
             ingredientName: ingredientName
         }).then(() => {
             alert('ingredient sent');
         })
-    }*/
-    
+    }
+    /*
+    //send data to server
     async function submitIngredient(e){
         e.preventDefault();
         try{
+            
             await axios.post('http://localhost:3001/api/post',
                { ingredientName: ingredientName });
+               //console.log(response);
         }catch(err){
             console.error(err);
         }
     }
-    return( 
+    */
+    //get data from server
+    //useEffect(() => {
+
+    
+       
+
+        return( 
+            <form className = "search_form" onSubmit={submitIngredient}>
+                <label>Search Bar</label>
+                <input
+                    type="text"
+                    placeholder="type your ingredient"
+                    name="ingredientName"
+                    onChange={ (e)=> {
+                        setIngredientName(e.target.value)
+                    }}
+                    
+                />
+                <button type="submit">Search</button>
+                {matchedRecipes.map((val)=>{
+                    return(
+                        <div>
+                            <ul>
+                                <li >Title: {val.title}</li>
+                                <li >Ingredient: {val.ingredients}</li>
+                                <li >Directions: {val.directions}</li>
+                            </ul>
+                        </div>
+                    )
+                    
+                })}
+            </form>   
+        );
+
+    
+    //})
+    /*return( 
         <form className = "search_form" onSubmit={submitIngredient}>
             <label>Search Bar</label>
             <input
@@ -35,8 +83,12 @@ const Search = () => {
                 
             />
             <button type="submit">Search</button>
+            
         </form>
-    )
+        
+    )*/
+    
 }
 
 export default Search;
+{"mode":"full","isActive":false}
