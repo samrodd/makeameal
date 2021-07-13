20131f7043ae07a7c86b0ae41269e2962d2d1240
anndu1 <48729176+anndu1@users.noreply.github.com>
2021-07-13 18:11:56 -0400

    added some frontend design

--- src/client/App.js
+++ src/client/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import Search from './search';
 
+
+
 function App() {
   return (
     <div className="App">
+      <div style={{
+        backgroundImage: `url("https://i.pinimg.com/originals/1d/93/e9/1d93e9df8121ff0c4c6dcb8650b101ec.jpg)`
+      }}>
       <h1>Make A Meal</h1>
       <h4>Create your meal now based on the ingredients you have at hand!</h4>
-
       <Search />
-      <input type="text" name="ingredient" />
+      </div>
     </div>
   );
 }
