a6823f800bf8a039998b6afca73e9e8302c98fc8
anndu1 <48729176+anndu1@users.noreply.github.com>
2021-07-16 22:04:28 -0400

    added some frontend styling design

--- src/client/App.js
+++ src/client/App.js
@@ -4,16 +4,23 @@ import Search from './search';
 
 
 
+
 function App() {
+  
   return (
     <div className="App">
-      <div style={{
-        backgroundImage: `url("https://i.pinimg.com/originals/1d/93/e9/1d93e9df8121ff0c4c6dcb8650b101ec.jpg)`
-      }}>
-      <h1>Make A Meal</h1>
-      <h4>Create your meal now based on the ingredients you have at hand!</h4>
-      <Search />
+      <div className="hero-image">
+      <div className="hero-text">
+
+        <h1>Make A Meal</h1>
+        <h3>Create your meal now based on the ingredients you have at hand!</h3>
+
       </div>
+      </div>
+
+      <Search />
+        
+        
     </div>
   );
 }
