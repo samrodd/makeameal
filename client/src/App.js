96090ab04362aad0f5015fc86f3498ab56bda95b
Jinxiang-Lin <78502763+Jinxiang-Lin@users.noreply.github.com>
2021-07-20 16:19:43 -0400

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,21 @@ import Search from './search';
 
 function App() {
   return (
-    <Search />
-  )
+    <div className="App">
+      <div className="hero-image">
+      <div className="hero-text">
+
+        <h1>Make A Meal</h1>
+        <h3>Create your meal now based on the ingredients you have at hand!</h3>
+
+      </div>
+      </div>
+
+      <Search />
+        
+        
+    </div>
+  );
 }
+
 export default App;
